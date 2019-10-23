66ecc088780861ba100a66a7e521d0c432894cc5
SonSeungWoo <skyghy3342@naver.com>
2019-10-23 16:02:09 +0900

    Initial commit

---
+++ src/main/java/me/seungwoo/alimtalk/AlimtalkApplication.java
@@ -0,0 +1,13 @@
+package me.seungwoo.alimtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimtalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlimtalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/seungwoo/alimtalk/AlimtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seungwoo.alimtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlimtalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
